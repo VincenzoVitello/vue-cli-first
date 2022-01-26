e0ca3df6eaa35c25fa09251b7eeabdf361c1bc95
Your Name <you@example.com>
2022-01-26 15:32:27 +0100

    Primo commit con errori di utilizzo componenti

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- contenuto del titolo -->
+    <img src="https://m.media-amazon.com/images/I/91McsWOj8sL._AC_SL1500_.jpg" alt="" class="jumboTron">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.jumboTron{
+  max-width: 800px;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- contenuto del titolo -->
+    <h1 class="titolo">Titolo</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Title',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.titolo{
+  color: green;
+}
+</style>
